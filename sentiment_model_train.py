f84cccd6d3b7c802bc06bc40497604dbccaaa420
Blazikengr8 <abhi.1414singh@gmail.com>
2021-09-26 09:58:20 +0530

    sentiment analysis

---
+++ sentiment_model_train.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+import time
+from sklearn import svm
+from sklearn.metrics import classification_report
+# train Data
+trainData = pd.read_csv("https://raw.githubusercontent.com/Vasistareddy/sentiment_analysis/master/data/train.csv")
+# test Data
+testData = pd.read_csv("https://raw.githubusercontent.com/Vasistareddy/sentiment_analysis/master/data/test.csv")
+# Create feature vectors
+vectorizer = TfidfVectorizer(min_df = 5,
+                             max_df = 0.8,
+                             sublinear_tf = True,
+                             use_idf = True)
+train_vectors = vectorizer.fit_transform(trainData['Content'])
+test_vectors = vectorizer.transform(testData['Content'])
+print(train_vectors,test_vectors,sep="\n")
+# Perform classification with SVM, kernel=linear
+classifier_linear = svm.SVC(kernel='linear')
+t0 = time.time()
+classifier_linear.fit(train_vectors, trainData['Label'])
+t1 = time.time()
+prediction_linear = classifier_linear.predict(test_vectors)
+t2 = time.time()
+time_linear_train = t1-t0
+time_linear_predict = t2-t1
+# results
+print("Training time: %fs; Prediction time: %fs" % (time_linear_train, time_linear_predict))
+report = classification_report(testData['Label'], prediction_linear, output_dict=True)
+print('positive: ', report['pos'])
+print('negative: ', report['neg'])
+# pickling the vectorizer
+pickle.dump(vectorizer, open('vectorizer.sav', 'wb'))
+# pickling the model
+pickle.dump(classifier_linear, open('classifier.sav', 'wb'))
