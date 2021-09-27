1e058826b073f0dc51f758f1b419343de6d6530a
Advaith123 <56351354+Advaith123@users.noreply.github.com>
2021-09-27 13:48:17 +0530

    Working Model

--- bot.py
+++ bot.py
@@ -1,11 +1,45 @@
 import discord
 from discord.ext import commands #.ext means extension presumably
 import Secret_Stuff
-
+import random
+import sentiment_analysis
 client = commands.Bot(command_prefix=".")
 
 @client.event #function decorator => basically says the next line is going to be a event function
 async def on_ready():
     print('Bot is ready!!')
+
+@client.event
+async def on_member_join(member):
+    print(f'{member} has joined a server.')
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member} has left a server.')
+
+@client.event
+async def on_message(message):
+    print(f'{message.author} has sent the message: {message.content}')
+    if sentiment_analysis.analyze(message.content) == 0:
+        mention = message.author.mention
+        response = f"hey {mention}, maybe chill a bit"
+        print(response)
+        await message.channel.send(response)
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+
+@client.command(aliases = ['8ball'])
+async def _8ball(ctx, *, question): #* allows to take multiple arguments
+    responses = ["As I see it, yes.", "Ask again later.", "Better not tell you now.", "Cannot predict now.",
+                 "Concentrate and ask again.",
+                 "Don’t count on it.", "It is certain.", "It is decidedly so.", "Most likely.", "My reply is no.",
+                 "My sources say no.",
+                 "Outlook not so good.", "Outlook good.", "Reply hazy, try again.", "Signs point to yes.",
+                 "Very doubtful.", "Without a doubt.",
+                 "Yes.", "Yes – definitely.", "You may rely on it."]
+    await ctx.send(f'Question: {question}\n Answer: {random.choice(responses)}')
+
 
 client.run(Secret_Stuff.token)
--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -4,28 +4,32 @@ from nltk.tokenize import word_tokenize
 from nltk.tag import pos_tag
 from nltk.stem import PorterStemmer
 import pandas as pd
-vectorizer = pickle.load(open('vectorizer.sav', 'rb'))
-classifier = pickle.load(open('classifier.sav', 'rb'))
-text=input()
-tokens = word_tokenize(text)
-tag = pos_tag(tokens)
-flag=False
-print(tag)
-ps=PorterStemmer()
-words=pd.read_csv('Excel_Swear_dic.csv')
-word=words['word']
-#print(word[0])
-res = any(ele in text for ele in word)
-for i in tag:       
-    if i[1] == 'PRP':
-        if not ps.stem(i[0]) == 'i' or ps.stem(i[0]) == 'me' or ps.stem(i[0]) == 'my':
-            flag=True           
-vector_text=vectorizer.transform([text])
-result=classifier.predict(vector_text)
-if flag==True and result==['neg'] and res == True:
-    print('negative')
-    print(flag,result,res)
-else:
-    print('positive')  
-    print(flag,result,res)  
+
+# responses = ['']
+
+def analyze(text):
+    vectorizer = pickle.load(open('vectorizer.sav', 'rb'))
+    classifier = pickle.load(open('classifier.sav', 'rb'))
+    # text=input()
+    tokens = word_tokenize(text)
+    tag = pos_tag(tokens)
+    flag=False #if pronoun other than I, me, mine etc (first person pronouns)
+    print(tag)
+    ps=PorterStemmer()
+    words=pd.read_csv('Excel_Swear_dic.csv')
+    word=words['word']
+    #print(word[0])
+    res = any(ele in text for ele in word) #profanity check (checks for slurs)
+    for i in tag:
+        if i[1] == 'PRP':
+            if not ps.stem(i[0]) == 'i' or ps.stem(i[0]) == 'me' or ps.stem(i[0]) == 'my':
+                flag=True
+    vector_text=vectorizer.transform([text])
+    result=classifier.predict(vector_text) #checks if the sentence is positive or negative
+    if flag==True and result==['neg'] and res == True:
+        return 0
+        print(flag,result,res)
+    else:
+        return 1
+        print(flag,result,res)
 #print(result)
