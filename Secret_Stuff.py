3a0c5e5fbc6b4b9240971d8e4e14b1a3baa1075d
Abhigyan Singh <60261112+Blazikengr8@users.noreply.github.com>
2021-11-13 14:30:34 +0530

    Update Secret_Stuff.py

--- Secret_Stuff.py
+++ Secret_Stuff.py
@@ -1,2 +1,2 @@
-token = 'ODkwNDU3OTUwMjY1Njg4MDc0.YUwFlw.Xmcdy1uEH-sT4VyU19XZprvuA40'
+token = 'add token here'
 invite_link = 'https://discord.com/api/oauth2/authorize?client_id=890457950265688074&permissions=0&scope=bot'
